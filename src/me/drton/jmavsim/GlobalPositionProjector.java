8905ad362a4fb40814c20240ece8d73a7b8ab926
Anton Babushkin <anton.babushkin@me.com>
2014-03-21 13:43:07 +0400

    GlobalPositionProjector cleanup

--- src/me/drton/jmavsim/GlobalPositionProjector.java
+++ src/me/drton/jmavsim/GlobalPositionProjector.java
@@ -1,16 +1,17 @@
 package me.drton.jmavsim;
 
+import static java.lang.Math.*;
+
 /**
  * User: ton Date: 11.07.13 Time: 22:11
  */
 public class GlobalPositionProjector {
     private boolean inited;
     private static double r_earth = 6371000.0;
-    private double phi_1;
-    private double sin_phi_1;
-    private double cos_phi_1;
-    private double lambda_0;
-    private double scale;
+    private double lat0;
+    private double lon0;
+    private double cos_lat0;
+    private double sin_lat0;
 
     public void reset() {
         inited = false;
@@ -20,69 +21,46 @@ public class GlobalPositionProjector {
         return inited;
     }
 
-    public void init(double lat_0, double lon_0) {
-        phi_1 = lat_0 / 180.0 * Math.PI;
-        lambda_0 = lon_0 / 180.0 * Math.PI;
-        sin_phi_1 = Math.sin(phi_1);
-        cos_phi_1 = Math.cos(phi_1);
-        double lat1 = phi_1;
-        double lon1 = lambda_0;
-        double lat2 = phi_1 + 0.5 / 180 * Math.PI;
-        double lon2 = lambda_0 + 0.5 / 180 * Math.PI;
-        double sin_lat_2 = Math.sin(lat2);
-        double cos_lat_2 = Math.cos(lat2);
-        double d = Math.acos(Math.sin(lat1) * sin_lat_2 + Math.cos(lat1) * cos_lat_2 * Math.cos(lon2 - lon1)) * r_earth;
-        double k_bar = 0;
-        double c = Math.acos(sin_phi_1 * sin_lat_2 + cos_phi_1 * cos_lat_2 * Math.cos(lon2 - lambda_0));
-        if (0 != c)
-            k_bar = c / Math.sin(c);
-        double x2 = k_bar * (cos_lat_2 * Math.sin(lon2 - lambda_0)); //Projection of point 2 on plane
-        double y2 = k_bar * ((cos_phi_1 * sin_lat_2 - sin_phi_1 * cos_lat_2 * Math.cos(lon2 - lambda_0)));
-        double rho = Math.sqrt(Math.pow(x2, 2) + Math.pow(y2, 2));
-        scale = d / rho;
+    public void init(double lat, double lon) {
         inited = true;
+        lat0 = lat * PI / 180.0;
+        lon0 = lon * PI / 180.0;
+        cos_lat0 = cos(lat0);
+        sin_lat0 = sin(lat0);
     }
 
     public double[] project(double lat, double lon) {
         if (!inited)
             throw new RuntimeException("Not initialized");
-        double phi = lat / 180.0 * Math.PI;
-        double lambda = lon / 180.0 * Math.PI;
-        double sin_phi = Math.sin(phi);
-        double cos_phi = Math.cos(phi);
-        double k_bar = 0;
-        double c = Math.acos(sin_phi_1 * sin_phi + cos_phi_1 * cos_phi * (1 - Math.pow((lambda - lambda_0), 2) / 2));
-        if (0 != c)
-            k_bar = c / Math.sin(c);
-        double y = k_bar * (cos_phi * (lambda - lambda_0)) *
-                scale;
-        double x = k_bar * ((cos_phi_1 * sin_phi - sin_phi_1 * cos_phi * (1 - Math.pow((lambda - lambda_0), 2) / 2))) *
-                scale;
+        double lat_rad = lat * PI / 180.0;
+        double lon_rad = lon * PI / 180.0;
+        double sin_lat = sin(lat_rad);
+        double cos_lat = cos(lat_rad);
+        double cos_d_lon = cos(lon_rad - lon0);
+        double c = acos(sin_lat0 * sin_lat + cos_lat0 * cos_lat * cos_d_lon);
+        double k = (c == 0.0) ? 1.0 : (c / sin(c));
+        double y = k * cos_lat * sin(lon_rad - lon0) * r_earth;
+        double x = k * (cos_lat0 * sin_lat - sin_lat0 * cos_lat * cos_d_lon) * r_earth;
         return new double[]{x, y};
     }
 
     public double[] reproject(double x, double y) {
         if (!inited)
             throw new RuntimeException("Not initialized");
-        double x_descaled = x / scale;
-        double y_descaled = y / scale;
-        double c = Math.sqrt(x_descaled * x_descaled + y_descaled * y_descaled);
-        double sin_c = Math.sin(c);
-        double cos_c = Math.cos(c);
-        double lat_sphere = 0;
-        if (c != 0)
-            lat_sphere = Math.asin(cos_c * sin_phi_1 + (x_descaled * sin_c * cos_phi_1) / c);
-        else
-            lat_sphere = Math.asin(cos_c * sin_phi_1);
-        double lon_sphere = 0;
-        if (phi_1 == Math.PI / 2) {
-            lon_sphere = (lambda_0 - y_descaled / x_descaled);
-        } else if (phi_1 == -Math.PI / 2) {
-            lon_sphere = (lambda_0 + y_descaled / x_descaled);
+        double x_rad = x / r_earth;
+        double y_rad = y / r_earth;
+        double c = sqrt(x_rad * x_rad + y_rad * y_rad);
+        double sin_c = sin(c);
+        double cos_c = cos(c);
+        double lat_rad;
+        double lon_rad;
+        if (c != 0.0) {
+            lat_rad = asin(cos_c * sin_lat0 + (x_rad * sin_c * cos_lat0) / c);
+            lon_rad = (lon0 + atan2(y_rad * sin_c, c * cos_lat0 * cos_c - x_rad * sin_lat0 * sin_c));
         } else {
-            lon_sphere = (lambda_0 +
-                    Math.atan2(y_descaled * sin_c, c * cos_phi_1 * cos_c - x_descaled * sin_phi_1 * sin_c));
+            lat_rad = lat0;
+            lon_rad = lon0;
         }
-        return new double[]{lat_sphere * 180.0 / Math.PI, lon_sphere * 180.0 / Math.PI};
+        return new double[]{lat_rad * 180.0 / Math.PI, lon_rad * 180.0 / Math.PI};
     }
 }
