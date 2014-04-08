5b17fab7ee87eb452987192c3297da96f40590a1
Anton Babushkin <anton.babushkin@me.com>
2014-04-08 20:34:47 +0400

    CameraGimbal2D: control camera pitch

--- src/me/drton/jmavsim/CameraGimbal2D.java
+++ src/me/drton/jmavsim/CameraGimbal2D.java
@@ -1,12 +1,18 @@
 package me.drton.jmavsim;
 
+import me.drton.jmavsim.vehicle.AbstractVehicle;
+
+import javax.vecmath.Matrix3d;
 import javax.vecmath.Vector3d;
+import java.util.List;
 
 /**
  * User: ton Date: 21.03.14 Time: 23:22
  */
 public class CameraGimbal2D extends MechanicalObject {
     private MechanicalObject baseObject;
+    private int pitchChannel = -1;
+    private double pitchScale = 1.0;
 
     public CameraGimbal2D(World world) {
         super(world);
@@ -16,6 +22,14 @@ public class CameraGimbal2D extends MechanicalObject {
         this.baseObject = object;
     }
 
+    public void setPitchChannel(int pitchChannel) {
+        this.pitchChannel = pitchChannel;
+    }
+
+    public void setPitchScale(double pitchScale) {
+        this.pitchScale = pitchScale;
+    }
+
     @Override
     public void update(long t) {
         this.position = baseObject.position;
@@ -23,6 +37,15 @@ public class CameraGimbal2D extends MechanicalObject {
         this.acceleration = baseObject.acceleration;
         double yaw = Math.atan2(baseObject.rotation.getElement(1, 0), baseObject.rotation.getElement(0, 0));
         this.rotation.rotZ(yaw);
+        if (pitchChannel >= 0 && baseObject instanceof AbstractVehicle) {
+            // Control camera pitch
+            List<Double> control = ((AbstractVehicle) baseObject).getControl();
+            if (control.size() > pitchChannel) {
+                Matrix3d r = new Matrix3d();
+                r.rotY(control.get(4) * pitchScale);
+                this.rotation.mul(r);
+            }
+        }
     }
 
     @Override
