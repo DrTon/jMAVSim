850ab2bbd7415b2f4129db048ce4bf51bb896da7
Anton Babushkin <anton.babushkin@me.com>
2014-06-13 14:08:47 +0200

    LogPlayerTarget: more flexible, supports any LogReader now with configurable field names

--- src/me/drton/jmavsim/LogPlayerTarget.java
+++ src/me/drton/jmavsim/LogPlayerTarget.java
@@ -1,8 +1,9 @@
 package me.drton.jmavsim;
 
+import me.drton.jmavlib.geo.GlobalPositionProjector;
+import me.drton.jmavlib.geo.LatLonAlt;
 import me.drton.jmavlib.log.FormatErrorException;
 import me.drton.jmavlib.log.LogReader;
-import me.drton.jmavlib.log.PX4LogReader;
 
 import javax.vecmath.Vector3d;
 import java.io.EOFException;
@@ -20,14 +21,34 @@ public class LogPlayerTarget extends Target {
     private long timeStart = 0;
     private long logT = 0;
     private Vector3d positionOffset = new Vector3d();
+    private String[] posKeys = new String[]{"LPOS.X", "LPOS.Y", "LPOS.Z"};
+    private String[] velKeys = new String[]{"LPOS.VX", "LPOS.VY", "LPOS.VZ"};
+    private boolean globalFrame = false;
+    private GlobalPositionProjector projector = null;
+    private Vector3d postitionPrev = new Vector3d();
+    private long timePrev = 0;
 
     public LogPlayerTarget(World world, double size) throws FileNotFoundException {
         super(world, size);
     }
 
-    void openLog(String fileName) throws IOException, FormatErrorException {
-        logReader = new PX4LogReader(fileName);
+    public void openLog(LogReader logReader) {
+        this.logReader = logReader;
         logStart = timeStart - logReader.getStartMicroseconds() / 1000;
+    }
+
+    public void setLogKeys(String[] posKeys, String[] velKeys) {
+        this.posKeys = posKeys;
+        this.velKeys = velKeys;
+    }
+
+    public void setGlobalFrame(boolean globalFrame) {
+        this.globalFrame = globalFrame;
+        this.projector = new GlobalPositionProjector();
+    }
+
+    public void setGlobalReference(LatLonAlt reference) {
+        this.projector.init(reference);
     }
 
     public void setTimeStart(long timeStart) {
@@ -55,17 +76,38 @@ public class LogPlayerTarget extends Target {
                     break;
                 }
             }
-            if (logData.containsKey("LPOS.X") &&
-                    logData.containsKey("LPOS.Y") &&
-                    logData.containsKey("LPOS.Z")) {
-                position.add(new Vector3d((Float) logData.get("LPOS.X"), (Float) logData.get("LPOS.Y"),
-                        (Float) logData.get("LPOS.Z")), positionOffset);
+            if (logData.containsKey(posKeys[0]) &&
+                    logData.containsKey(posKeys[1]) &&
+                    logData.containsKey(posKeys[2])) {
+                double[] v = new double[]{
+                        ((Number) logData.get(posKeys[0])).doubleValue(),
+                        ((Number) logData.get(posKeys[1])).doubleValue(),
+                        ((Number) logData.get(posKeys[2])).doubleValue()};
+                if (globalFrame) {
+                    LatLonAlt latLonAlt = new LatLonAlt(v[0], v[1], v[2]);
+                    if (!projector.isInited()) {
+                        projector.init(latLonAlt);
+                    }
+                    position.add(new Vector3d(projector.project(latLonAlt)), positionOffset);
+                } else {
+                    position.add(new Vector3d(v), positionOffset);
+                }
+                if (velKeys == null) {
+                    // Calculate velocity from position changes
+                    velocity.sub(position, postitionPrev);
+                    velocity.scale(1000.0 / (logT - timePrev));
+                    postitionPrev.set(position);
+                    timePrev = logT;
+                }
             }
-            if (logData.containsKey("LPOS.VX") &&
-                    logData.containsKey("LPOS.VY") &&
-                    logData.containsKey("LPOS.VZ")) {
-                velocity.set((Float) logData.get("LPOS.VX"), (Float) logData.get("LPOS.VY"),
-                        (Float) logData.get("LPOS.VZ"));
+            if (velKeys != null) {
+                // Use velocity from log
+                if (logData.containsKey(velKeys[0]) &&
+                        logData.containsKey(velKeys[1]) &&
+                        logData.containsKey(velKeys[2])) {
+                    velocity.set((Float) logData.get(velKeys[0]), (Float) logData.get(velKeys[1]),
+                            (Float) logData.get(velKeys[2]));
+                }
             }
         }
         super.update(t);
